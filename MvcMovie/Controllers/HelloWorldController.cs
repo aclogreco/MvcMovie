d95cba6f8ea5d188f7912a1315a59f18aca8d90d
Anthony LoGreco <aclogreco@gmail.com>
2014-09-20 03:06:41 -0400

    Modify HelloWorldController Welcome method so that it uses the default id parameter along with the query string parameter name.

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -12,8 +12,8 @@ namespace MvcMovie.Controllers {
 		}
 
 		// GET: /HelloWorld/Welcome/
-		public string Welcome(string name, int numTimes = 1) {
-			return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+		public string Welcome(string name, int ID = 1) {
+			return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
 		}
 	}
 }
