c1021a737e4d0a46162e264a9bf897ebff8b2581
Anthony LoGreco <aclogreco@gmail.com>
2014-11-23 04:44:45 -0500

    Add a search by genre feature to the Movies controller.

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -13,12 +13,24 @@ namespace MvcMovie.Controllers {
 		private MovieDBContext db = new MovieDBContext();
 
 		// GET: Movies
-		public ActionResult Index (string searchString) {
+		public ActionResult Index (string movieGenre, string searchString) {
+			var GenreList = new List<string>();
+
+			var GenreQuery = from d in db.Movies
+						  orderby d.Genre
+						  select d.Genre;
+			GenreList.AddRange(GenreQuery.Distinct());
+			ViewBag.movieGenre = new SelectList(GenreList);
+
 			var movies = from m in db.Movies
 					   select m;
 
 			if (!String.IsNullOrEmpty(searchString)) {
 				movies = movies.Where(s => s.Title.Contains(searchString));
+			}
+
+			if (!String.IsNullOrEmpty(movieGenre)) {
+				movies = movies.Where(x => x.Genre == movieGenre);
 			}
 
 			return View(movies);
